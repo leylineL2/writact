de718486a959a8e8b9e56511e07c1bcda5d93fc1
leyla <leyla@astralfield.blue>
2022-12-02 11:07:19 +0900

    convert mili second add dict function

--- writact/record.py
+++ writact/record.py
@@ -1,10 +1,21 @@
-from typing import Union
+from typing import Union, TypedDict
 from datetime import datetime
 from decimal import Decimal
 import logging
 logger = logging.getLogger(__name__)
 logger.addHandler(logging.NullHandler())
 
+class IRecord(TypedDict):
+  timestamp: str
+  action: str
+  source: str
+  base: str
+  volume: str
+  price: str
+  counter: str
+  fee: str
+  feeccy: str
+  comment: str
 
 class Record:
   def __init__(self)->None:
@@ -40,6 +51,8 @@ class Record:
       except(Exception) as e:
         raise(e)
     elif(type(value) == int):
+      if(len(value)==13):
+        value = value/1000
       self._timestamp = datetime.fromtimestamp(value)
     else:
       raise(Exception,"not supported value")
@@ -68,9 +81,15 @@ class Record:
   def fee(self,value:int|str|Decimal):
     self._fee = str(value)
 
+  def get_datetime_from_timestamp(self)->datetime:
+    return self._timestamp
 
   def insert_timestamp_from_iso(self,value)->None:
     self._timestamp = datetime.fromisoformat(value)
+
+  def to_dict(self)->TypedDict[IRecord]:
+    result = {'timestamp':self.timestamp,'action':self.action,'source':self.source,'base':self.base,'volume':self.volume,'price':self.price,'counter':self.counter,'fee':self.fee,'feeccy':self.feeccy,'comment':self.comment}
+    return result
 
   def get_cryptact_line(self,demilita = ',')->str:
     result = demilita.join([self.timestamp,self.action,self.source,self.base,self.volume,self.price,self.counter,self.fee,self.feeccy,self.comment])
